25bb1c788cde82f5efcfd6bac0e345a38d3d0068
Dan Hahne <contact@danhahne.com>
2019-11-04 13:19:09 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='textgrid_utils',
+    version='0.0.1',
+    description='utilities for working with textgrid files',
+    url='https://github.com/giphahne/textgrid-utils',
+    author='Dan Hahne',
+    author_email='contact@danhahne.com',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    keywords='linguistics textgrid praat',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    install_requires=['argcomplete'],
+    package_data={
+        '': ['package_data.dat'],
+    },
+)
